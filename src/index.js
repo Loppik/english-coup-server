d9ad0fe8217847b3ae5aab2c0b2c4767db7c64b0
Loppik <shantyr_qq@mail.ru>
2019-01-31 16:46:03 +0300

    added init db tables

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+const initDatabase = require('./initDatabase');
+initDatabase.initDatabaseTables();
+
 app.listen(config.port, () => {
   console.log(`Server listening on http://${config.host}:${config.port}`)
 })
---
+++ src/initDatabase.js
@@ -0,0 +1,21 @@
+const { Client } = require('pg');
+const config = require('./configs/db');
+
+const client = new Client({
+  user: config.user,
+  host: config.host,
+  database: config.database,
+  password: config.password,
+})
+client.connect()
+
+exports.initDatabaseTables = () => {
+  try {
+    client.query("CREATE TABLE IF NOT EXISTS users ( user_id INT PRIMARY KEY, email VARCHAR(30), password VARCHAR(20) )");
+    client.query("CREATE TABLE IF NOT EXISTS status ( status_id INT PRIMARY KEY, name VARCHAR(30) )");
+    client.query("CREATE TABLE IF NOT EXISTS userWords ( word_id INT PRIMARY KEY, status_id INT, user_id INT )");
+    client.query("CREATE TABLE IF NOT EXISTS words ( word_id INT PRIMARY KEY, original VARCHAR(50), translation VARCHAR(50), custom BOOLEAN )");
+  } catch(err) {
+    console.log(err);
+  }
+}
