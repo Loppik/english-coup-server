1ac09fc5ce8202e9c34e9a7bb720797308ae14a6
Loppik <shantyr_qq@mail.ru>
2019-02-01 16:49:53 +0300

    table of words changed

--- src/initDatabase.js
+++ src/initDatabase.js
@@ -14,7 +14,7 @@ exports.initDatabaseTables = () => {
     client.query("CREATE TABLE IF NOT EXISTS users ( user_id INT PRIMARY KEY, email VARCHAR(30), password VARCHAR(20) )");
     client.query("CREATE TABLE IF NOT EXISTS status ( status_id INT PRIMARY KEY, name VARCHAR(30) )");
     client.query("CREATE TABLE IF NOT EXISTS userWords ( word_id INT PRIMARY KEY, status_id INT, user_id INT )");
-    client.query("CREATE TABLE IF NOT EXISTS words ( word_id INT PRIMARY KEY, original VARCHAR(50), translation VARCHAR(50), custom BOOLEAN )");
+    client.query("CREATE TABLE IF NOT EXISTS words ( word_id SERIAL PRIMARY KEY, original VARCHAR(50), translation VARCHAR(50), custom BOOLEAN )");
   } catch(err) {
     console.log(err);
   }
